d32fd33422ca7c3d1562733a685bfe6f25358af9
surya76657 <surya76657@gmail.com>
2019-08-20 08:56:05 +0530

    monor css fixes

--- src/app/video.service.ts
+++ src/app/video.service.ts
@@ -22,17 +22,10 @@ export class VideoService {
     );
   }
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
-
 
       // Let the app keep running by returning an empty result.
       return of(result as T);
