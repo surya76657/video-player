79c85849d2d3ddcc1f503a6bc230d62845e7a12f
surya76657 <surya76657@gmail.com>
2019-08-23 00:55:05 +0530

    minor changes in server file

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path')
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 4200, (PORT) => {
-  console.log(`Our app is running on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/video-player/index.html'));
 })
 
 console.log('console Listening')
